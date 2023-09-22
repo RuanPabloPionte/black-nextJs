776debd6fed198b77ad8ad178400f2a423262de9
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-09-22 19:41:00 -0300

    basic fetch of a test api

--- src/app/dynamic/page.tsx
+++ src/app/dynamic/page.tsx
@@ -1,8 +1,25 @@
-// "use client";
+"use client";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 // import { Col, Container, Row } from "reactstrap";
 
+type apiReponse = {
+  name: string;
+  timestamp: Date;
+};
+
 const Dynamic: NextPage = () => {
+  const [clientData, setClientData] = useState<apiReponse>();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const clientData = await fetch("/api/hello").then((res) => res.json());
+    setClientData(clientData);
+  };
+
   return (
     <main className="container">
       <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
@@ -13,7 +30,7 @@ const Dynamic: NextPage = () => {
         </div>
 
         <div className="col">
-          <h3>Gerado no cliente:</h3>
+          <h3>Gerado no cliente: {clientData?.name}</h3>
         </div>
       </div>
     </main>
