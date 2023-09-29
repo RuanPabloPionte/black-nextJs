c4d26849e03417d3b3361c0f727758495da7e23d
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-09-29 16:13:47 -0300

    creation of the hook for useCart context

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,6 +1,12 @@
-import Product from "@/app/products/[id]/page";
+"use client";
 import { ProductsType } from "@/services/product";
-import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 type CartContextType = {
   cart: ProductsType[];
@@ -8,38 +14,40 @@ type CartContextType = {
   removeProduct: (productId: number) => void;
 };
 
-const CartContext = createContext<CartContextType>({} as CartContextType)
+const CartContext = createContext<CartContextType>({} as CartContextType);
 
 export const CartContextProvider = (props: { children: ReactNode }) => {
   const [cart, setCart] = useState<ProductsType[]>(
     JSON.parse(localStorage.getItem("shopping-cart") || "[]")
   );
 
-  useEffect(()=>{
-    localStorage.setItem("shopping-cart", JSON.stringify(cart))
-  }, [cart])
+  useEffect(() => {
+    localStorage.setItem("shopping-cart", JSON.stringify(cart));
+  }, [cart]);
 
-  const addProduct = (product: ProductsType)=>{
-    const updatedCart = [...cart, product]
-    setCart(updatedCart)
-  }
+  const addProduct = (product: ProductsType) => {
+    const updatedCart = [...cart, product];
+    setCart(updatedCart);
+  };
 
-  const removeProduct = (productId: number) =>{
-    const removedProduct = cart.find(product => productId === Number(product.id))
-    if(confirm(`Tem certeza que quer remover o itme ${removedProduct.name}`)){
-        const updatedCart = cart.filter(product => productId !== Number(product.id))
-        setCart(updatedCart)
+  const removeProduct = (productId: number) => {
+    const removedProduct = cart.find(
+      (product) => productId === Number(product.id)
+    );
+    if (
+      confirm(`Tem certeza que quer remover o itme ${removedProduct?.name}`)
+    ) {
+      const updatedCart = cart.filter(
+        (product) => productId !== Number(product.id)
+      );
+      setCart(updatedCart);
     }
-
-    return(
-        <CartContext.Provider
-        value={{ cart, addProduct, removeProduct }}
-      >
-        {props.children}
-      </CartContext.Provider>
-    )
-
-  }
+  };
+  return (
+    <CartContext.Provider value={{ cart, addProduct, removeProduct }}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
-export const useCart = () => useContext(CartContext)
+export const useCart = () => useContext(CartContext);
