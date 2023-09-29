b93ad23f13a48988e24554b617bb497338d1789c
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-09-29 10:40:52 -0300

    creation of the addProduct function

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -13,5 +13,15 @@ export const CartContextProvider = (props: { children: ReactNode }) => {
   const [cart, setCart] = useState<ProductsType[]>(
     JSON.parse(localStorage.getItem("shopping-cart") || "[]")
   );
+
+  useEffect(()=>{
+    localStorage.setItem("shopping-cart", JSON.stringify(cart))
+  }, [cart])
+
+  const addProduct = (product: ProductsType)=>{
+    const updatedCart = [...cart, product]
+    setCart(updatedCart)
+  }
+
 };
 
